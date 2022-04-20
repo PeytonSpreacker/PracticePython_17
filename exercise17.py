71e7f5759c3e079ee9e0eef07f9e23d4690f5c07
PeytonSpreacker <peyton@spreacker.com>
2022-04-19 21:04:04 -0500

    ugh

--- exercise17.py
+++ exercise17.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-baseurl = 'https://www.nytimes.com/'
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-response=requests.get(baseurl,headers=headers)
-soup=BeautifulSoup(response.content,'html.parser')
-print(response)
+url = 'http://www.nytimes.com'
+r = requests.get(url)
+r_html = r.text
+# print(r_html)
+
+soup = BeautifulSoup(r_html, 'html.parser')
+
+for titles in soup.find_all(class_="story_wrapper indicate-hover css-18cyl96"):
+	title = titles.a
+	print(title.string)
 
 # r = requests.get(baseurl)
 # soup = BeautifulSoup(r.text, 'html.parser')
