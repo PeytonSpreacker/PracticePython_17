b5ad7dc62dbd1af80d643c0cf45434cf39e20588
PeytonSpreacker <peyton@spreacker.com>
2022-04-19 20:56:30 -0500

    continued work...

--- exercise17.py
+++ exercise17.py
@@ -2,11 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 baseurl = 'https://www.nytimes.com/'
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+response=requests.get(baseurl,headers=headers)
+soup=BeautifulSoup(response.content,'html.parser')
+print(response)
 
-r = requests.get(baseurl)
-soup = BeautifulSoup(r.text, 'html.parser')
+# r = requests.get(baseurl)
+# soup = BeautifulSoup(r.text, 'html.parser')
 
-posts = soup.select('section:not(section[data-testid="block-Briefings"]) article')
+# posts = soup.select('section:not(section[data-testid="block-Briefings"]) article')
 
-for i, post in enumerate(posts, 1):
-    print('{: <4}{}'.format(str(i) + '.', post.find('h2').text))
+# for i, post in enumerate(posts, 1):
+#     print('{: <4}{}'.format(str(i) + '.', post.find('h2').text))
