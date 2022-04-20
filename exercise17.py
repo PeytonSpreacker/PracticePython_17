88dea2c92f1cf7e4fe7363c10cdf1bfb8b082ab7
PeytonSpreacker <peyton@spreacker.com>
2022-04-19 20:50:23 -0500

    commit - learn CSS selectors..

--- exercise17.py
+++ exercise17.py
@@ -1,3 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+baseurl = 'https://www.nytimes.com/'
+
+r = requests.get(baseurl)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+posts = soup.select('section:not(section[data-testid="block-Briefings"]) article')
+
+for i, post in enumerate(posts, 1):
+    print('{: <4}{}'.format(str(i) + '.', post.find('h2').text))
